e2db76edf081ebc94e19ee124be93910f5ff3f47
Ankit6321 <ankityadav133it@gmail.com>
2025-08-23 19:38:33 +0530

    some corrections2

--- app/src/main/java/com/example/quizupsignup/MainActivity3.kt
+++ app/src/main/java/com/example/quizupsignup/MainActivity3.kt
@@ -10,7 +10,7 @@ class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.register)
+        setContentView(R.layout.forgot_password)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
