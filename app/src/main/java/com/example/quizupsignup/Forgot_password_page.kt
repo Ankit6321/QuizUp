18418572cb8e8a8d489e8cd88877003f426487ee
Ankit6321 <ankityadav133it@gmail.com>
2025-08-24 03:14:08 +0530

    made the host page

--- app/src/main/java/com/example/quizupsignup/Forgot_password_page.kt
+++ app/src/main/java/com/example/quizupsignup/Forgot_password_page.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity2 : AppCompatActivity() {
+class Forgot_password_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.home)
+        setContentView(R.layout.forgot_password)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
