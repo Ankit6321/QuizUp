f03a8378432e300afc169b0d3c072b8269460ed6
Ankit6321 <ankityadav133it@gmail.com>
2025-08-24 17:07:07 +0530

    .

--- app/src/main/java/com/example/quizupsignup/Forgot_password_page.kt
+++ app/src/main/java/com/example/quizupsignup/Forgot_password_page.kt
@@ -11,7 +11,7 @@ class Forgot_password_page : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.forgot_password)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.forgot_page)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
--- app/src/main/java/com/example/quizupsignup/MainActivity.kt
+++ app/src/main/java/com/example/quizupsignup/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.quizupsignup
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnlogin: AppCompatButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        btnlogin = findViewById(R.id.btnlogin)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnlogin.setOnClickListener {
+            val intent = Intent(this@MainActivity, Home_page::class.java)
+            startActivity(intent)
         }
     }
 }
